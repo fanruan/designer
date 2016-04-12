54961579ec33458bfc022a67996d319cc1112a8c
vito <vito@finereport.com>
2016-04-13 00:22:04 +0800

    注释漏传了

--- designer/src/com/fr/design/mainframe/bbs/BBSDialog.java
+++ designer/src/com/fr/design/mainframe/bbs/BBSDialog.java
@@ -142,6 +142,9 @@ public class BBSDialog extends UIDialog {
         }
     }
 
+    /**
+     * 提供给web页面调用的关闭窗口
+     */
     public void closeWindow() {
         this.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
         this.setVisible(false);
