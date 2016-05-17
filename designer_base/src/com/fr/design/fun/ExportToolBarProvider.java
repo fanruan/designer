eefa01daaefdafd974dd9aaa8ca5a5d104810434
richie <richie@finereport.com>
2016-05-17 14:29:17 +0800

    单元格Tag增量修改

--- designer_base/src/com/fr/design/fun/ExportToolBarProvider.java
+++ designer_base/src/com/fr/design/fun/ExportToolBarProvider.java
@@ -6,17 +6,12 @@ import javax.swing.*;
 
 /**
  * 导出菜单设计器端拓展，用于控制该菜单是否在web端显示
- * @author focus
- * @date Jul 2, 2015
- * @since 8.0
  */
 public interface ExportToolBarProvider extends Level{
 	
 	String XML_TAG = "ExportToolBarProvider";
 
 	int CURRENT_LEVEL = 1;
-
-
 
 	/**
 	 *
@@ -28,16 +23,12 @@ public interface ExportToolBarProvider extends Level{
 	JPanel updateCenterPane(JPanel pane);
 	
 	/**
-	 * 根据xml里面存的web段按钮显示状态更新对应的checkbox
-	 * 
+	 * 更新界面
 	 */
 	void populate();
 	
 	/**
-	 * 根据checkbox控制web段菜单是否显示
-	 * 
-	 * @return
+	 * 保存界面设置
 	 */
 	void update();
-	
 }
--- designer_base/src/com/fr/design/fun/HighlightProvider.java
+++ designer_base/src/com/fr/design/fun/HighlightProvider.java
@@ -5,17 +5,13 @@ import com.fr.design.condition.ConditionAttributesPane;
 import com.fr.stable.fun.Level;
 
 /**
- * @author richie
- * @date 2015-03-26
- * @since 8.0
- * 条件属性接口
+ * 条件属性界面设计接口
  */
 public interface HighlightProvider extends Level{
 
     String MARK_STRING = "HighlightProvider";
 
     int CURRENT_LEVEL = 1;
-
 
     /**
      * 条件属性的实现类
