e6295278f45261dcb041feaa54b3e18f5afa6459
vito <vito@finereport.com>
2016-05-20 17:44:43 +0800

    1、删除插件返回一个成功消息，供界面显示 2、更新过期插件弹出两个对话框的问题

--- designer_base/src/com/fr/design/extra/exe/UpdateOnlineExecutor.java
+++ designer_base/src/com/fr/design/extra/exe/UpdateOnlineExecutor.java
@@ -10,6 +10,7 @@ import com.fr.design.extra.Process;
 import com.fr.general.Inter;
 import com.fr.plugin.Plugin;
 import com.fr.plugin.PluginLoader;
+import com.fr.plugin.PluginVerifyException;
 import com.fr.stable.StringUtils;
 
 import javax.swing.*;
@@ -43,44 +44,51 @@ public class UpdateOnlineExecutor implements Executor {
 
                     @Override
                     public void run(Process<String> process) {
-                        if(StringUtils.isBlank(DesignerEnvManager.getEnvManager().getBBSName())){
+                        if (StringUtils.isBlank(DesignerEnvManager.getEnvManager().getBBSName())) {
                             LoginCheckContext.fireLoginCheckListener();
                         }
-                        if(StringUtils.isNotBlank(DesignerEnvManager.getEnvManager().getBBSName())){
-                            for (int i = 0; i < pluginIDs.length; i++) {
-                                Plugin plugin = PluginLoader.getLoader().getPluginById(pluginIDs[i]);
-                                String id = null;
-                                if (plugin != null) {
-                                    id = plugin.getId();
-                                }
-                                String username = DesignerEnvManager.getEnvManager().getBBSName();
-                                String password = DesignerEnvManager.getEnvManager().getBBSPassword();
-                                try {
-                                    PluginHelper.downloadPluginFile(id, username, password, new Process<Double>() {
-                                        @Override
-                                        public void process(Double integer) {
+                        if (StringUtils.isNotBlank(DesignerEnvManager.getEnvManager().getBBSName())) {
+                            try {
+                                for (int i = 0; i < pluginIDs.length; i++) {
+                                    try {
+                                        Plugin plugin = PluginLoader.getLoader().getPluginById(pluginIDs[i]);
+                                        String id = null;
+                                        if (plugin != null) {
+                                            id = plugin.getId();
                                         }
-                                    });
-                                    updateFileFromDisk(PluginHelper.getDownloadTempFile());
-                                    process.process(PERCENT_100 / pluginIDs.length * (i + 1) + "%");
-                                } catch (Exception e) {
-                                    FRContext.getLogger().error(e.getMessage(), e);
+                                        String username = DesignerEnvManager.getEnvManager().getBBSName();
+                                        String password = DesignerEnvManager.getEnvManager().getBBSPassword();
+                                        PluginHelper.downloadPluginFile(id, username, password, new Process<Double>() {
+                                            @Override
+                                            public void process(Double integer) {
+                                            }
+                                        });
+                                        updateFileFromDisk(PluginHelper.getDownloadTempFile());
+                                        process.process(PERCENT_100 / pluginIDs.length * (i + 1) + "%");
+                                    } catch (Exception e) {
+                                        if (e instanceof PluginVerifyException) {
+                                            throw (PluginVerifyException) e;
+                                        }
+                                        FRContext.getLogger().error(e.getMessage());
+                                    }
                                 }
-                            }
-                            int rv = JOptionPane.showOptionDialog(
-                                    null,
-                                    Inter.getLocText("FR-Designer-Plugin_Update_Successful"),
-                                    Inter.getLocText("FR-Designer-Plugin_Warning"),
-                                    JOptionPane.YES_NO_OPTION,
-                                    JOptionPane.INFORMATION_MESSAGE,
-                                    null,
-                                    new String[]{Inter.getLocText("FR-Designer-Basic_Restart_Designer"),
-                                            Inter.getLocText("FR-Designer-Basic_Restart_Designer_Later")
-                                    },
-                                    null
-                            );
-                            if (rv == JOptionPane.OK_OPTION) {
-                                RestartHelper.restart();
+                                int rv = JOptionPane.showOptionDialog(
+                                        null,
+                                        Inter.getLocText("FR-Designer-Plugin_Update_Successful"),
+                                        Inter.getLocText("FR-Designer-Plugin_Warning"),
+                                        JOptionPane.YES_NO_OPTION,
+                                        JOptionPane.INFORMATION_MESSAGE,
+                                        null,
+                                        new String[]{Inter.getLocText("FR-Designer-Basic_Restart_Designer"),
+                                                Inter.getLocText("FR-Designer-Basic_Restart_Designer_Later")
+                                        },
+                                        null
+                                );
+                                if (rv == JOptionPane.OK_OPTION) {
+                                    RestartHelper.restart();
+                                }
+                            } catch (PluginVerifyException e) {
+                                JOptionPane.showMessageDialog(null, e.getMessage(), Inter.getLocText("FR-Designer-Plugin_Warning"), JOptionPane.ERROR_MESSAGE);
                             }
                         }
                     }
@@ -88,26 +96,22 @@ public class UpdateOnlineExecutor implements Executor {
         };
     }
 
-    private void updateFileFromDisk(File fileOnDisk) {
-        try {
-            Plugin plugin = PluginHelper.readPlugin(fileOnDisk);
-            if (plugin == null) {
-                JOptionPane.showMessageDialog(null, Inter.getLocText("FR-Designer-Plugin_Illegal_Plugin_Zip"), Inter.getLocText("FR-Designer-Plugin_Warning"), JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-            Plugin oldPlugin = PluginLoader.getLoader().getPluginById(plugin.getId());
-            if (oldPlugin != null) {
-                String[] files = PluginHelper.uninstallPlugin(FRContext.getCurrentEnv(), oldPlugin);
-                PluginHelper.installPluginFromUnzippedTempDir(FRContext.getCurrentEnv(), plugin, new After() {
-                    @Override
-                    public void done() {
-                    }
-                });
-            } else {
-                JOptionPane.showMessageDialog(null, Inter.getLocText("FR-Designer-Plugin_Cannot_Update_Not_Install"), Inter.getLocText("FR-Designer-Plugin_Warning"), JOptionPane.ERROR_MESSAGE);
-            }
-        } catch (Exception e1) {
-            JOptionPane.showMessageDialog(null, e1.getMessage(), Inter.getLocText("FR-Designer-Plugin_Warning"), JOptionPane.ERROR_MESSAGE);
+    private void updateFileFromDisk(File fileOnDisk) throws Exception {
+        Plugin plugin = PluginHelper.readPlugin(fileOnDisk);
+        if (plugin == null) {
+            JOptionPane.showMessageDialog(null, Inter.getLocText("FR-Designer-Plugin_Illegal_Plugin_Zip"), Inter.getLocText("FR-Designer-Plugin_Warning"), JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        Plugin oldPlugin = PluginLoader.getLoader().getPluginById(plugin.getId());
+        if (oldPlugin != null) {
+            String[] files = PluginHelper.uninstallPlugin(FRContext.getCurrentEnv(), oldPlugin);
+            PluginHelper.installPluginFromUnzippedTempDir(FRContext.getCurrentEnv(), plugin, new After() {
+                @Override
+                public void done() {
+                }
+            });
+        } else {
+            JOptionPane.showMessageDialog(null, Inter.getLocText("FR-Designer-Plugin_Cannot_Update_Not_Install"), Inter.getLocText("FR-Designer-Plugin_Warning"), JOptionPane.ERROR_MESSAGE);
         }
     }
 }
