51bdaf47b6150f2155ab611c97b2bac6178f2a1f
vito <vito@finereport.com>
2016-05-24 12:10:04 +0800

    无窗口设计

--- designer_base/src/com/fr/design/extra/PluginShopDialog.java
+++ designer_base/src/com/fr/design/extra/PluginShopDialog.java
@@ -4,17 +4,23 @@ import com.fr.design.dialog.BasicPane;
 import com.fr.design.dialog.UIDialog;
 import com.fr.design.utils.gui.GUICoreUtils;
 import com.fr.general.Inter;
+import javafx.embed.swing.JFXPanel;
 
+import javax.swing.*;
 import java.awt.*;
 
 /**
  * Created by vito on 16/4/18.
  */
 public class PluginShopDialog extends UIDialog {
-    private static final Dimension DEFAULT_SHOP = new Dimension(900, 684);
+    private static final Dimension DEFAULT_SHOP = new Dimension(900, 700);
 
     public PluginShopDialog(Frame frame, BasicPane pane) {
-        super(frame, pane, false);
+        super(frame);
+        setUndecorated(true);
+        JPanel panel = (JPanel) getContentPane();
+        panel.setLayout(new BorderLayout());
+        add(pane, BorderLayout.CENTER);
         setSize(DEFAULT_SHOP);
         GUICoreUtils.centerWindow(this);
         setResizable(false);
