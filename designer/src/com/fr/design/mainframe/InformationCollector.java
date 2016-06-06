e8cc320b88a0bbd58311a3946f0c1fb6e3ba629c
neil <neil@finereport.com>
2016-06-06 09:18:05 +0800

    24小时传一次

--- designer/src/com/fr/design/mainframe/InformationCollector.java
+++ designer/src/com/fr/design/mainframe/InformationCollector.java
@@ -35,8 +35,8 @@ import java.util.*;
  */
 public class InformationCollector implements XMLReadable, XMLWriter {
 	
-	//3天上传一次
-	private static final long DELTA = 3 * 24 * 3600 * 1000L;
+	// 24小时上传一次
+	private static final long DELTA = 24 * 3600 * 1000L;
 	private static final long SEND_DELAY = 30 * 1000L;
 	private static final String FILE_NAME = "fr.info";
 	private static final String XML_START_STOP_LIST = "StartStopList";
