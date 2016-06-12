30a159a114fe836d253066c7f80360a719d2156d
xiaohu <xiaohu@finereport.com>
2016-06-12 11:18:35 +0800

    update

--- designer_base/src/com/fr/design/editor/editor/ColumnSelectedEditor.java
+++ designer_base/src/com/fr/design/editor/editor/ColumnSelectedEditor.java
@@ -37,7 +37,7 @@ public class ColumnSelectedEditor extends Editor<SimpleDSColumn> {
 
 			@Override
 			public void itemStateChanged(ItemEvent e) {
-                //這邊需要重新初始化columnNames, 否則nameList長度和columnNames長度不同導致出錯。
+                //这边需要重新初始化columnNames, 否则nameList长度和columnNames长度不同导致出錯。
                 List<String> nameList = tableDataComboBox.getSelectedItem().calculateColumnNameList();
                 columnNames = new String[nameList.size()];
 				columnNames = tableDataComboBox.getSelectedItem().calculateColumnNameList().toArray(columnNames);
