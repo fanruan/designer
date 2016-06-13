758e1f73adc45d24f80cd97518c90d46e3f9556c
Lee <xintu@LeetekiMacBook-Pro.local>
2016-06-13 23:02:26 +0800

    基础控件类抽取需要的代码单独作一个方法，在子类中重写

--- designer_form/src/com/fr/design/mainframe/widget/editors/DoubleEditor.java
+++ designer_form/src/com/fr/design/mainframe/widget/editors/DoubleEditor.java
@@ -1,11 +1,34 @@
 package com.fr.design.mainframe.widget.editors;
 
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.text.NumberFormat;
+import java.text.ParseException;
 
 public class DoubleEditor extends FormattedEditor {
 
+    private JFormattedTextField textField;
+
     public DoubleEditor() {
         super(NumberFormat.getNumberInstance());
+        textField = (JFormattedTextField) super.getCustomEditor();
+    }
+
+    @Override
+    public KeyListener createKeyListener() {
+       return new KeyAdapter() {
+
+           public void keyReleased(KeyEvent e) {
+               try {
+                   textField.commitEdit();
+                   return;
+               } catch (ParseException e1) {
+                   return;
+               }
+           }
+       };
     }
 
     @Override
