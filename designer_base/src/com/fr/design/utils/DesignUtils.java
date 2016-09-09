5435099b55beac706a69d6883bfdcd8c97c629c3
Fangjie Hu <Fangjie Hu>
2016-09-09 20:29:36 +0800

    update

--- designer_base/src/com/fr/design/utils/DesignUtils.java
+++ designer_base/src/com/fr/design/utils/DesignUtils.java
@@ -52,7 +52,7 @@ public class DesignUtils {
     public static boolean isStarted() {
         try {
             new Socket("localhost", port);
-            /*return true;*/
+            return true;
         } catch (Exception exp) {
 
         }
