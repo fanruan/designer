e523b23ac4af5dbe3ded24d174010782000a5217
PanLi320 <854954082@qq.com>
2016-10-08 18:22:51 +0800

    bugfix:私人消息的论坛账号同步

--- designer/src/com/fr/design/mainframe/bbs/UserInfoLabel.java
+++ designer/src/com/fr/design/mainframe/bbs/UserInfoLabel.java
@@ -87,6 +87,7 @@ public class UserInfoLabel extends UILabel{
 		LoginCheckContext.addLoginCheckListener(new LoginCheckListener() {
 			@Override
 			public void loginChecked() {
+				/*
 				if (bbsLoginDialog == null) {
 					bbsLoginDialog = new BBSLoginDialog(DesignerContext.getDesignerFrame(), UserInfoLabel.this);
 				}
@@ -94,6 +95,7 @@ public class UserInfoLabel extends UILabel{
 				bbsLoginDialog.showTipForDownloadPluginWithoutLogin();
 				bbsLoginDialog.setModal(true);
 				bbsLoginDialog.showWindow();
+				*/
 			}
 		});
 
@@ -282,7 +284,11 @@ public class UserInfoLabel extends UILabel{
 					public void mousePressed(MouseEvent e) {
 						if(StringUtils.isNotEmpty(userName)){
 							try {
-								Desktop.getDesktop().browse(new URI(SiteCenter.getInstance().acquireUrlByKind("bbs.default")));
+								String loginUrl = SiteCenter.getInstance().acquireUrlByKind("bbs.default") +
+										"&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=" +
+										"&username=" + DesignerEnvManager.getEnvManager().getBBSName() +
+										"&password=" + DesignerEnvManager.getEnvManager().getBBSPassword();
+								Desktop.getDesktop().browse(new URI(loginUrl));
 							} catch (Exception exp) {
 								FRContext.getLogger().info(exp.getMessage());
 							}
