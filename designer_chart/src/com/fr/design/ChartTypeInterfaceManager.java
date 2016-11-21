d070ef81e3297475c5353a2ec9265e058f02d568
Fangjie Hu <Fangjie Hu>
2016-11-21 17:43:45 +0800

    兼容老的插件

--- designer_chart/src/com/fr/design/ChartTypeInterfaceManager.java
+++ designer_chart/src/com/fr/design/ChartTypeInterfaceManager.java
@@ -274,8 +274,18 @@ public class ChartTypeInterfaceManager extends XMLFileManager implements ExtraCh
     public String getTitle4PopupWindow(String priority, String plotID){
         if (chartTypeInterfaces != null && chartTypeInterfaces.containsKey(priority) && chartTypeInterfaces.get(priority).containsKey(plotID)){
             IndependentChartUIProvider provider = chartTypeInterfaces.get(priority).get(plotID);
-            return   provider.getPlotTypePane().title4PopupWindow();
+            return provider.getPlotTypePane().title4PopupWindow();
+        }
 
+        //兼容老的插件
+        if (chartTypeInterfaces != null) {
+            Iterator iterator = chartTypeInterfaces.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry entry = (Map.Entry) iterator.next();
+                String defaultPriority = (String) entry.getKey();
+                if (chartTypeInterfaces.get(defaultPriority).containsKey(plotID))
+                return chartTypeInterfaces.get(defaultPriority).get(plotID).getPlotTypePane().title4PopupWindow();
+            }
         }
         return new String();
     }
