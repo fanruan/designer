4a78e4b8bb23696f34385d9f7da0726495b0af16
Fangjie Hu <Fangjie Hu>
2016-11-23 09:34:27 +0800

    选中的图表不能删除按钮

--- designer_chart/src/com/fr/design/mainframe/chart/gui/ChartTypeButtonPane.java
+++ designer_chart/src/com/fr/design/mainframe/chart/gui/ChartTypeButtonPane.java
@@ -62,20 +62,20 @@ public class ChartTypeButtonPane extends BasicBeanPane<ChartCollection> implemen
     private boolean pressOtherButtonWhenEditing = false;
 
 
-//    private AWTEventListener awt = new AWTEventListener() {
-//        public void eventDispatched(AWTEvent event) {
-//            //没有进行鼠标点击，则返回
-//            if (event instanceof MouseEvent && ((MouseEvent) event).getClickCount() > 0) {
-//                if (currentEditingEditor != null && !ComparatorUtils.equals(event.getSource(), currentEditingEditor)) {
-//                    stopEditing();
-//                    if (event.getSource() instanceof ChartChangeButton) {
-//                        ((ChartChangeButton) event.getSource()).mouseClick((MouseEvent) event);
-//                    }
-//                    populateBean(editingCollection);
-//                }
-//            }
-//        }
-//    };
+    /*private AWTEventListener awt = new AWTEventListener() {
+        public void eventDispatched(AWTEvent event) {
+            //没有进行鼠标点击，则返回
+            if (event instanceof MouseEvent && ((MouseEvent) event).getClickCount() > 0) {
+                if (currentEditingEditor != null && !ComparatorUtils.equals(event.getSource(), currentEditingEditor)) {
+                    stopEditing();
+                    if (event.getSource() instanceof ChartChangeButton) {
+                        ((ChartChangeButton) event.getSource()).mouseClick((MouseEvent) event);
+                    }
+                    populateBean(editingCollection);
+                }
+            }
+        }
+    };*/
 
     public ChartTypeButtonPane(ChartTypePane chartTypePane){
         this();
@@ -440,25 +440,22 @@ public class ChartTypeButtonPane extends BasicBeanPane<ChartCollection> implemen
         }
 
         private void deleteAButton() {
-            if (indexList.contains(this) && indexList.size() > 1) {
-                indexList.remove(this);
+            //先重构属性，在重构面板，否则面板在重构过程中，会重新将属性中的切换图表加到indexList中，导致面板无法删除
+            if (editingCollection != null) {
+                int count = editingCollection.getChartCount();
+                for (int i = 0; i < count; i++) {
+                    if (ComparatorUtils.equals(getButtonName(), editingCollection.getChartName(i))) {
+                        editingCollection.removeNameObject(i);
+                        break;
+                    }
+                }
+            }
 
+            indexList.remove(this);
+            if (indexList.contains(this) && indexList.size() > 1) {
                 if (this.isSelected()) {
                     indexList.get(0).setSelected(true);
                     changeCollectionSelected(indexList.get(0).getButtonName());
-                }
-
-                if (editingCollection != null) {
-                    int count = editingCollection.getChartCount();
-                    for (int i = 0; i < count; i++) {
-                        if (ComparatorUtils.equals(getButtonName(), editingCollection.getChartName(i))) {
-                            editingCollection.removeNameObject(i);
-                            if (i <= editingCollection.getSelectedIndex()){
-                                editingCollection.setSelectedIndex(editingCollection.getSelectedIndex()-1);
-                            }
-                            break;
-                        }
-                    }
                 }
             }
 
