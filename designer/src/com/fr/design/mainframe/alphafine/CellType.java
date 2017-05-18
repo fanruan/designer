b7e3a23d471d78bb1e78cec64c993fcb46c208bf
xiaoxia <xiaoxia@finereport.com>
2017-05-18 10:35:01 +0800

    加个方法

--- designer/src/com/fr/design/mainframe/alphafine/CellType.java
+++ designer/src/com/fr/design/mainframe/alphafine/CellType.java
@@ -6,13 +6,36 @@ package com.fr.design.mainframe.alphafine;
 public enum CellType {
     PLUGIN(0), DOCUMENT(1), FILE(2), ACTION(3), REUSE(4);
 
-
     private int cellType;
+
     CellType(int type) {
         this.cellType = type;
     }
 
+    public static CellType parse(int cellType) {
+        CellType type;
+        switch (cellType) {
+            case 0:
+                type = PLUGIN;
+                break;
+            case 1:
+                type = DOCUMENT;
+                break;
+            case 2:
+                type = FILE;
+                break;
+            case 3:
+                type = ACTION;
+                break;
+            case 4:
+                type = REUSE;
+                break;
+            default:
+                type = FILE;
+        }
+        return type;
 
+    }
     public int getCellType() {
         return cellType;
     }
