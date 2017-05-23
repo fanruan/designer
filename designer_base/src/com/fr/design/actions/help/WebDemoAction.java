f05df0b65e7233fc7d789936fba1da3f8aded452
eason-skx <eason.skx@gmail.com>
2017-05-23 16:50:37 +0800

    pmd

--- designer_base/src/com/fr/design/actions/help/WebDemoAction.java
+++ designer_base/src/com/fr/design/actions/help/WebDemoAction.java
@@ -34,7 +34,7 @@ public class WebDemoAction extends UpdateAction {
 
         @Override
         public String getMenuName() {
-            return Inter.getLocText("Product_Demo");
+            return Inter.getLocText("FR-Product_Demo");
         }
 
         @Override
