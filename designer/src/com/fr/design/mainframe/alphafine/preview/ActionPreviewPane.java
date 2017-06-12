bfdf0e71e59a827bd58587e041d0c1c02e2062b2
XiaXiang <xiaoxia@finereport.com>
2017-06-12 17:10:40 +0800

    bug fix

--- designer/src/com/fr/design/mainframe/alphafine/preview/ActionPreviewPane.java
+++ designer/src/com/fr/design/mainframe/alphafine/preview/ActionPreviewPane.java
@@ -3,8 +3,8 @@ package com.fr.design.mainframe.alphafine.preview;
 
 import com.fr.design.gui.ilable.UILabel;
 import com.fr.design.mainframe.alphafine.AlphaFineConstants;
-import com.fr.design.mainframe.alphafine.AlphaFineHelper;
 import com.fr.general.IOUtils;
+import com.fr.general.Inter;
 
 import javax.swing.*;
 import java.awt.*;
@@ -18,19 +18,19 @@ public class ActionPreviewPane extends JPanel {
     public ActionPreviewPane() {
         setLayout(new BorderLayout());
         setBackground(null);
-        setBorder(BorderFactory.createEmptyBorder(120,0,0,0));
+        setBorder(BorderFactory.createEmptyBorder(135,0,0,0));
         UILabel image = new UILabel();
         image.setPreferredSize(new Dimension(150,111));
         image.setHorizontalAlignment(SwingConstants.CENTER);
         image.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
         image.setIcon(IOUtils.readIcon("/com/fr/design/mainframe/alphafine/images/noresult.png"));
-        UILabel name = new UILabel("暂不支持显示");
-        name.setForeground(AlphaFineConstants.MEDIUM_GRAY);
-        name.setFont(NAME);
-        name.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
-        name.setHorizontalAlignment(SwingConstants.CENTER);
+        UILabel description = new UILabel(Inter.getLocText("FR-Designer_NoResult"));
+        description.setForeground(AlphaFineConstants.MEDIUM_GRAY);
+        description.setFont(NAME);
+        description.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+        description.setHorizontalAlignment(SwingConstants.CENTER);
         this.add(image, BorderLayout.CENTER);
-        this.add(name, BorderLayout.SOUTH);
+        this.add(description, BorderLayout.SOUTH);
     }
 
 }
