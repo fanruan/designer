a52695fea6daafec8e46c06d96f7765f0af0a638
hzzz <hzzz0823@gmail.com>
2017-08-02 16:04:36 +0800

    format

--- designer_base/src/com/fr/design/gui/itoolbar/UIToolbar.java
+++ designer_base/src/com/fr/design/gui/itoolbar/UIToolbar.java
@@ -1,35 +1,38 @@
 package com.fr.design.gui.itoolbar;
 
+import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
-import javax.swing.JToolBar;
+public class UIToolbar extends JToolBar {
+    private static final int TOOLBAR_HEIGNT = 26;
 
-public class UIToolbar extends JToolBar{
-	private static final int TOOLBAR_HEIGNT = 26;
-	public UIToolbar() {
-		this(FlowLayout.LEFT);
-	}
+    public UIToolbar() {
+        this(FlowLayout.LEFT);
+    }
 
-	public UIToolbar(int align) {
-		super();
-		setFloatable(false);
-	    setRollover(true);
-	    setLayout(new FlowLayout(align, 4, 0));
-	    setUI(new UIToolBarUI());
-	    setBorderPainted(false);
-	    setBackground(Color.RED);
-	}
-	
-	@Override
-	public Dimension getPreferredSize() {
-		Dimension dim = super.getPreferredSize();
-		dim.height = TOOLBAR_HEIGNT;
-		return dim;
-	}
+    public UIToolbar(int align, UIToolBarUI uiToolBarUI) {
+        super();
+        setFloatable(false);
+        setRollover(true);
+        setLayout(new FlowLayout(align, 4, 0));
+        setUI(uiToolBarUI);
+        setBorderPainted(false);
+    }
 
-    public void checkComponentsByNames (boolean flag, ArrayList<String> names) {
-        for (int i = 0; i < getComponentCount(); i ++) {
+    public UIToolbar(int align) {
+        this(align, new UIToolBarUI());
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        Dimension dim = super.getPreferredSize();
+        dim.height = TOOLBAR_HEIGNT;
+        return dim;
+    }
+
+    public void checkComponentsByNames(boolean flag, ArrayList<String> names) {
+        for (int i = 0; i < getComponentCount(); i++) {
             Component component = getComponents()[i];
             if (names.contains(component.getName())) {
                 component.setEnabled(flag);
