c785bce4d8277795220ffd1c1cf18d7cf7cd625b
kerry <kerry@finereport.com>
2017-08-09 11:05:23 +0800

    界面调整

--- designer/src/com/fr/design/widget/ui/CheckBoxGroupDefinePane.java
+++ designer/src/com/fr/design/widget/ui/CheckBoxGroupDefinePane.java
@@ -61,7 +61,7 @@ public class CheckBoxGroupDefinePane extends FieldEditorDefinePane<CheckBoxGroup
 		double[] columnSize = {p, f};
 		int[][] rowCount = {{1, 1},{1, 1},{1,1},{1,1}};
 		JPanel panel =  TableLayoutHelper.createGapTableLayoutPane(components, rowSize, columnSize, rowCount, 10, 7);
-		panel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
 		advancePane.add(panel);
 		return advancePane;
 
