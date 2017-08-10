dd58e3ff4764fcc0e3805985c6732d41dc8e1374
fanglei <294531121>
2017-08-10 15:46:55 +0800

    还是修改国际化文件

--- designer_form/src/com/fr/design/widget/ui/designer/mobile/ElementCaseDefinePane.java
+++ designer_form/src/com/fr/design/widget/ui/designer/mobile/ElementCaseDefinePane.java
@@ -72,8 +72,8 @@ public class ElementCaseDefinePane extends MobileWidgetDefinePane{
         this.designer = WidgetPropertyPane.getInstance().getEditingFormDesigner();
         this.hComboBox = new UIComboBox(ITEMS);
         this.vComboBox = new UIComboBox(ITEMS);
-        this.heightRestrictCheckBox = new UICheckBox(Inter.getLocText("Form-EC_heightrestrict"));
-        this.maxHeightLabel = new UILabel(Inter.getLocText("Form-EC_heightpercent"), SwingConstants.LEFT);
+        this.heightRestrictCheckBox = new UICheckBox(Inter.getLocText("FR-Designer_Mobile-Height-Limit"));
+        this.maxHeightLabel = new UILabel(Inter.getLocText("FR-Designer_Mobile-Height-Percent"), SwingConstants.LEFT);
         this.maxHeightSpinner = new UISpinner(0, 1, 0.01, 0.75);
         maxHeightSpinner.setVisible(false);
         maxHeightLabel.setVisible(false);
