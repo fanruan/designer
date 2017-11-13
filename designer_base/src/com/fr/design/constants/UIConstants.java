764fc3b455e41cf76454526a62dc7fdd7dbec42c
plough <plough@fanruan.com>
2017-11-13 16:07:30 +0800

    REPORT-5284 公测客户反馈设计器配色和交互问题修改=>微调弹窗标题

--- designer_base/src/com/fr/design/constants/UIConstants.java
+++ designer_base/src/com/fr/design/constants/UIConstants.java
@@ -98,6 +98,7 @@ public interface UIConstants {
     public static final Color ATTRIBUTE_NORMAL = Color.WHITE;
     public static final Color ATTRIBUTE_HOVER = new Color(0xF5F5F7);
     public static final Color UI_TOOLBAR_COLOR = new Color(0xF5F5F7);
+    public static final Color DIALOG_TITLEBAR_BACKGROUND = new Color(0xFCFCFD);
     public static final Color CHECKBOX_HOVER_SELECTED = new Color(0x3394f0);
     public static final Color TEXT_FILED_BORDER_SELECTED = new Color(0x3384f0);
     public static final Color SHEET_NORMAL = new Color(0xc8c8ca);
