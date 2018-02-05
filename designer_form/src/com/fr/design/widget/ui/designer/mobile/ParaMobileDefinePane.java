5f993e58aa6d8ae3506f303227b4b09f25e2cee9
plough <plough@fanruan.com>
2018-02-05 13:53:15 +0800

    MOBILE-3963 9.0设计器新界面（移动端）=>参数面板

---
+++ designer_form/src/com/fr/design/widget/ui/designer/mobile/ParaMobileDefinePane.java
@@ -0,0 +1,102 @@
+package com.fr.design.widget.ui.designer.mobile;
+
+import com.fr.design.designer.beans.events.DesignerEvent;
+import com.fr.design.designer.creator.XCreator;
+import com.fr.design.foldablepane.UIExpandablePane;
+import com.fr.design.gui.frpane.AttributeChangeListener;
+import com.fr.design.gui.icheckbox.UICheckBox;
+import com.fr.design.layout.FRGUIPaneFactory;
+import com.fr.design.mainframe.FormDesigner;
+import com.fr.design.mainframe.MobileWidgetListPane;
+import com.fr.design.mainframe.WidgetPropertyPane;
+import com.fr.general.Inter;
+import com.fr.stable.StringUtils;
+
+import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.Method;
+
+/**
+ * Created by plough on 2018/2/5.
+ */
+public class ParaMobileDefinePane extends MobileWidgetDefinePane {
+    private XCreator paraCreator;
+    private FormDesigner designer;
+    private AttributeChangeListener changeListener;
+    private UICheckBox appRelayoutCheck;
+    private MobileWidgetListPane mobileWidgetListPane;
+
+    public ParaMobileDefinePane(XCreator xCreator) {
+        this.paraCreator = xCreator;
+    }
+
+    @Override
+    protected void initContentPane() {}
+
+    @Override
+    protected JPanel createContentPane() {
+        return new JPanel();
+    }
+
+    @Override
+    public String getIconPath() {
+        return StringUtils.EMPTY;
+    }
+
+    @Override
+    public String title4PopupWindow() {
+        return StringUtils.EMPTY;
+    }
+
+
+    @Override
+    public void initPropertyGroups(Object source) {
+        this.setLayout(FRGUIPaneFactory.createBorderLayout());
+        this.designer = WidgetPropertyPane.getInstance().getEditingFormDesigner();
+        this.add(getMobileWidgetListPane(), BorderLayout.CENTER);
+        this.repaint();
+    }
+
+    // 控件顺序
+    private UIExpandablePane getMobileWidgetListPane() {
+        mobileWidgetListPane = new MobileWidgetListPane(designer);
+        mobileWidgetListPane.setBorder(BorderFactory.createEmptyBorder(10, 0, 5, 0));
+        JPanel panelWrapper = FRGUIPaneFactory.createBorderLayout_S_Pane();
+        panelWrapper.add(mobileWidgetListPane, BorderLayout.CENTER);
+
+        return new UIExpandablePane("控件顺序", 280, 20, panelWrapper);
+    }
+
+    private void bindListeners2Widgets() {
+        reInitAllListeners();
+        this.changeListener = new AttributeChangeListener() {
+            @Override
+            public void attributeChange() {
+                update();
+            }
+        };
+    }
+
+    /**
+     * 后台初始化所有事件.
+     */
+    private void reInitAllListeners() {
+        initListener(this);
+    }
+
+
+    @Override
+    public void populate(FormDesigner designer) {
+        this.designer = designer;
+
+        // 设置监听
+        this.bindListeners2Widgets();
+        this.addAttributeChangeListener(changeListener);
+    }
+
+    @Override
+    public void update() {
+        mobileWidgetListPane.updateToDesigner();
+        designer.getEditListenerTable().fireCreatorModified(DesignerEvent.CREATOR_EDITED);
+    }
+}
