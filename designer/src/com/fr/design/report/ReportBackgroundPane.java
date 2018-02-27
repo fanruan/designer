080283bff53a7abf2c7a6424eed827c23f72d050
zack <zack@finreport.com>
2018-02-27 21:25:59 +0800

    REPORT-6997 纸张背景设置为不可导出和打印时，导出PDF还是会导出纸张背景

--- designer/src/com/fr/design/report/ReportBackgroundPane.java
+++ designer/src/com/fr/design/report/ReportBackgroundPane.java
@@ -1,16 +1,18 @@
 package com.fr.design.report;
 
-import java.awt.BorderLayout;
-
-import com.fr.page.ReportSettingsProvider;
+import com.fr.design.dialog.BasicPane;
 import com.fr.design.gui.icheckbox.UICheckBox;
 import com.fr.design.layout.FRGUIPaneFactory;
-import com.fr.design.dialog.BasicPane;
-import com.fr.general.Inter;
 import com.fr.design.style.background.BackgroundPane;
+import com.fr.general.Inter;
+import com.fr.page.ReportSettingsProvider;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
 
 public class ReportBackgroundPane extends BasicPane {
     private UICheckBox isPrintBackgroundCheckBox;
+    private UICheckBox isExportBackgroundCheckBox;
     private BackgroundPane backgroundPane;
 
     public ReportBackgroundPane() {
@@ -19,14 +21,17 @@ public class ReportBackgroundPane extends BasicPane {
         backgroundPane = new BackgroundPane();
         this.add(backgroundPane, BorderLayout.CENTER);
 
-        isPrintBackgroundCheckBox = new UICheckBox(
-                Inter.getLocText("ReportGUI-Print_Background"));
-        this.add(isPrintBackgroundCheckBox, BorderLayout.SOUTH);
+        isPrintBackgroundCheckBox = new UICheckBox(Inter.getLocText("FR-Designer_Print_Background"));
+        isExportBackgroundCheckBox = new UICheckBox(Inter.getLocText("FR-Designer_Export_Background"));
+        JPanel sourth = new JPanel();
+        sourth.add(isExportBackgroundCheckBox);
+        sourth.add(isPrintBackgroundCheckBox);
+        this.add(sourth, BorderLayout.SOUTH);
     }
-    
+
     @Override
     protected String title4PopupWindow() {
-    	return Inter.getLocText(new String[]{"paper", "Background"});
+        return Inter.getLocText(new String[]{"paper", "Background"});
     }
 
     /**
@@ -35,6 +40,7 @@ public class ReportBackgroundPane extends BasicPane {
     public void populate(ReportSettingsProvider reportSettings) {
         this.backgroundPane.populate(reportSettings.getBackground());
         this.isPrintBackgroundCheckBox.setSelected(reportSettings.isPrintBackground());
+        this.isExportBackgroundCheckBox.setSelected(reportSettings.isExportBackground());
     }
 
     /**
@@ -43,5 +49,6 @@ public class ReportBackgroundPane extends BasicPane {
     public void update(ReportSettingsProvider reportSettings) {
         reportSettings.setBackground(this.backgroundPane.update());
         reportSettings.setPrintBackground(this.isPrintBackgroundCheckBox.isSelected());
+        reportSettings.setExportBackground(this.isExportBackgroundCheckBox.isSelected());
     }
 }
