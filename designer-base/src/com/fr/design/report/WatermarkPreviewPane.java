458ffcf0f5ffd523afc3627ed5d9d24a6ea479a6
plough <plough@fanruan.com>
2018-05-23 15:35:15 +0800

    REPORT-7222 安全性之水印=>支持公式

--- designer-base/src/com/fr/design/report/WatermarkPreviewPane.java
+++ designer-base/src/com/fr/design/report/WatermarkPreviewPane.java
@@ -32,6 +32,6 @@ public class WatermarkPreviewPane extends JPanel {
         super.paint(g);
         Graphics2D g2d = (Graphics2D)g;
         g2d.drawImage(UIConstants.WATERMARK_BACKGROUND, 0, 0, this.getWidth(), this.getHeight(), null);
-        WatermarkPainter.getInstance().paint(watermark, g2d, this.getWidth(), this.getHeight());
+        WatermarkPainter.getInstance().paint(watermark, g2d, this.getWidth(), this.getHeight(), true);
     }
 }
