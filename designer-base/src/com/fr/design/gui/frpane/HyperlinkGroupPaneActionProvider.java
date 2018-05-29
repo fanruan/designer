2eaeba9f964ea531d19d43be00cedc7340fc932f
yaoh.wu <yaoh.wu@fanruan.com>
2018-05-29 19:04:24 +0800

    REPORT-6949 条件属性的超级链接重命名后会自动生成超级链接

--- designer-base/src/com/fr/design/gui/frpane/HyperlinkGroupPaneActionProvider.java
+++ designer-base/src/com/fr/design/gui/frpane/HyperlinkGroupPaneActionProvider.java
@@ -3,9 +3,23 @@ package com.fr.design.gui.frpane;
 import com.fr.design.designer.TargetComponent;
 
 /**
+ * @author plough
  * Created by plough on 2017/7/26.
  */
 public interface HyperlinkGroupPaneActionProvider {
+
+    /**
+     * 刷新面板展示
+     *
+     * @param hyperlinkGroupPane 超链面板
+     * @param elementCasePane    模板
+     */
     void populate(HyperlinkGroupPane hyperlinkGroupPane, TargetComponent elementCasePane);
+
+    /**
+     * 保存到文件
+     *
+     * @param hyperlinkGroupPane 超联面板
+     */
     void saveSettings(HyperlinkGroupPane hyperlinkGroupPane);
 }
