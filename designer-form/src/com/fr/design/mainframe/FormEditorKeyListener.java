e356bfb4874149c13ba32789d79e163cef3fceec
plough <plough@fanruan.com>
2018-06-07 16:40:41 +0800

    REPORT-8355 9.0之前开发的功能与bug patch到10.0（部分）

--- designer-form/src/com/fr/design/mainframe/FormEditorKeyListener.java
+++ designer-form/src/com/fr/design/mainframe/FormEditorKeyListener.java
@@ -1,5 +1,10 @@
 package com.fr.design.mainframe;
 
+import com.fr.base.BaseUtils;
+import com.fr.common.inputevent.InputEventBaseOnOS;
+import java.awt.Cursor;
+import java.awt.Point;
+import java.awt.Toolkit;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -10,6 +15,9 @@ import com.fr.design.designer.creator.XLayoutContainer;
 import com.fr.design.designer.creator.XWAbsoluteLayout;
 
 public class FormEditorKeyListener extends KeyAdapter{
+	private static final Cursor ADDCURSOR = Toolkit.getDefaultToolkit().createCustomCursor(
+			BaseUtils.readImage("/com/fr/design/images/form/designer/cursor/add.png"), new Point(0, 0),
+			"addCursor");
 	private FormDesigner designer;
 	private boolean moved;
 
@@ -19,6 +27,9 @@ public class FormEditorKeyListener extends KeyAdapter{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+		if(e.isShiftDown() || InputEventBaseOnOS.isControlDown(e)) {
+			designer.setCursor(ADDCURSOR);
+		}
 		int code = e.getKeyCode();
 		XCreator creator = designer.getSelectionModel().getSelection().getSelectedCreator();
 		XLayoutContainer container;
@@ -26,26 +37,29 @@ public class FormEditorKeyListener extends KeyAdapter{
 			return;
 		}
 		moved = true;
-		
+
 		switch (code) {
-		case KeyEvent.VK_LEFT:
-			designer.getSelectionModel().move(-1, 0);
-			break;
-		case KeyEvent.VK_RIGHT:
-			designer.getSelectionModel().move(1, 0);
-			break;
-		case KeyEvent.VK_UP:
-			designer.getSelectionModel().move(0, -1);
-			break;
-		case KeyEvent.VK_DOWN:
-			designer.getSelectionModel().move(0, 1);
-			break;
-		default:
-			moved = false;
+			case KeyEvent.VK_LEFT:
+				designer.getSelectionModel().move(-1, 0);
+				break;
+			case KeyEvent.VK_RIGHT:
+				designer.getSelectionModel().move(1, 0);
+				break;
+			case KeyEvent.VK_UP:
+				designer.getSelectionModel().move(0, -1);
+				break;
+			case KeyEvent.VK_DOWN:
+				designer.getSelectionModel().move(0, 1);
+				break;
+			default:
+				moved = false;
 		}
 	}
-	
+
 	public void keyReleased(KeyEvent e) {
+		if(!(e.isShiftDown() || InputEventBaseOnOS.isControlDown(e))) {
+			designer.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		}
 		if (moved) {
 			designer.getEditListenerTable().fireCreatorModified(
 					designer.getSelectionModel().getSelection().getSelectedCreator(), DesignerEvent.CREATOR_RESIZED);
---
+++ designer-form/src/com/fr/design/mainframe/MobileWidgetListPane.java
@@ -0,0 +1,71 @@
+package com.fr.design.mainframe;
+
+import com.fr.design.designer.creator.XCreator;
+import com.fr.design.gui.controlpane.UISimpleListControlPane;
+import com.fr.form.ui.Widget;
+import com.fr.form.ui.container.WSortLayout;
+import com.fr.general.NameObject;
+import com.fr.stable.ArrayUtils;
+import com.fr.stable.Nameable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by plough on 2018/1/31.
+ */
+public class MobileWidgetListPane extends UISimpleListControlPane {
+    public static final String LIST_NAME = "Widget_List";
+
+    private FormDesigner designer;
+    private WSortLayout wSortLayout;
+    private String[] widgetNameList;
+
+    public MobileWidgetListPane(FormDesigner designer, WSortLayout wSortLayout) {
+        super();
+        this.designer = designer;
+        this.wSortLayout = wSortLayout;
+        widgetNameList = getData();
+
+        List<NameObject> nameObjectList = new ArrayList<NameObject>();
+        for (String name : widgetNameList) {
+            nameObjectList.add(new NameObject(name, null));
+        }
+        populate(nameObjectList.toArray(new NameObject[nameObjectList.size()]));
+    }
+
+    /**
+     * 保存移动端控件列表顺序
+     */
+    public void updateToDesigner() {
+        Nameable[] nameableList = update();
+        List<String> newMobileWidgetList = new ArrayList<>();
+        for (Nameable nameable : nameableList) {
+            newMobileWidgetList.add(nameable.getName());
+        }
+        wSortLayout.updateSortedMobileWidgetList(newMobileWidgetList);
+    }
+
+    /**
+     * 获取选中控件的控件列表
+     *
+     * @return List<String> widgetNameList
+     */
+    private String[] getData() {
+        //选择的控件
+        XCreator selectedCreator = designer.getSelectionModel().getSelection().getSelectedCreator();
+        Widget selectedModel = selectedCreator != null ? selectedCreator.toData() : null;
+
+        if (selectedModel == null || !selectedModel.acceptType(WSortLayout.class)) {
+            return ArrayUtils.EMPTY_STRING_ARRAY;
+        }
+
+        // 选择的控件有两种类型，一种是WLayout，代表容器，一种是Widget，代表控件
+        List<String> mobileWidgetList = ((WSortLayout) selectedModel).getOrderedMobileWidgetList();
+        String[] widgetNames = new String[mobileWidgetList.size()];
+        for (int i = 0; i < mobileWidgetList.size(); i++) {
+            widgetNames[i] = mobileWidgetList.get(i);
+        }
+        return widgetNames;
+    }
+}
