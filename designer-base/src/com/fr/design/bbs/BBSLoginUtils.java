43d8270a47a9b9df5b8970164c01d43bea61f10b
kerry <kerry@finereport.com>
2018-06-08 10:22:05 +0800

    REPORT-8630	插件管理的国际化都有问题(先改回去，后面统一改下国际化)

--- designer-base/src/com/fr/design/bbs/BBSLoginUtils.java
+++ designer-base/src/com/fr/design/bbs/BBSLoginUtils.java
@@ -1,8 +1,11 @@
 package com.fr.design.bbs;
 
-import com.fr.base.FRContext;
+import com.fr.config.Configuration;
 import com.fr.config.MarketConfig;
+import com.fr.log.FineLoggerFactory;
 import com.fr.stable.StringUtils;
+import com.fr.transaction.Configurations;
+import com.fr.transaction.Worker;
 
 import java.util.List;
 
@@ -11,26 +14,46 @@ import java.util.List;
  */
 public class BBSLoginUtils {
 
-    public static void bbsLogin(String username, String password) {
+    public static void bbsLogin(final String username, final String password) {
         try {
-            MarketConfig.getInstance().setBbsUsername(username);
-            MarketConfig.getInstance().setBbsPassword(password);
+            Configurations.update(new Worker() {
+                @Override
+                public void run() {
+                    MarketConfig.getInstance().setBbsUsername(username);
+                    MarketConfig.getInstance().setBbsPassword(password);
+                }
+                @Override
+                public Class<? extends Configuration>[] targets() {
+                    return new Class[]{MarketConfig.class};
+                }
+            });
+
         } catch (Exception e) {
-            FRContext.getLogger().error(e.getMessage());
+            FineLoggerFactory.getLogger().error(e.getMessage());
         }
     }
 
     public static void bbsLogin(List<String> list) {
         try {
-            String uid = list.get(0);
-            String username = list.get(1);
-            String password = list.get(2);
-            MarketConfig.getInstance().setBbsUsername(username);
-            MarketConfig.getInstance().setBbsPassword(password);
-            MarketConfig.getInstance().setBbsUid(Integer.parseInt(uid));
-            MarketConfig.getInstance().setInShowBBsName(username);
+            final String uid = list.get(0);
+            final String username = list.get(1);
+            final String password = list.get(2);
+            Configurations.update(new Worker() {
+                @Override
+                public void run() {
+                    MarketConfig.getInstance().setBbsUsername(username);
+                    MarketConfig.getInstance().setBbsPassword(password);
+                    MarketConfig.getInstance().setBbsUid(Integer.parseInt(uid));
+                    MarketConfig.getInstance().setInShowBBsName(username);
+                }
+                @Override
+                public Class<? extends Configuration>[] targets() {
+                    return new Class[]{MarketConfig.class};
+                }
+            });
+
         } catch (Exception e) {
-            FRContext.getLogger().error(e.getMessage());
+            FineLoggerFactory.getLogger().error(e.getMessage());
         }
     }
 
@@ -41,7 +64,7 @@ public class BBSLoginUtils {
             MarketConfig.getInstance().setBbsUid(0);
             MarketConfig.getInstance().setInShowBBsName(StringUtils.EMPTY);
         } catch (Exception e) {
-            FRContext.getLogger().error(e.getMessage());
+            FineLoggerFactory.getLogger().error(e.getMessage());
         }
     }
 }
