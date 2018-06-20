382ddcc505b87746d96de0eab505f5962299b350
Mata.Li <Mata.Li@fanruan.com>
2018-06-20 18:24:43 +0800

    REPORT-8860 and REPORT-6379

--- designer-base/src/com/fr/design/hyperlink/WebHyperNorthPane.java
+++ designer-base/src/com/fr/design/hyperlink/WebHyperNorthPane.java
@@ -2,6 +2,7 @@ package com.fr.design.hyperlink;
 
 import com.fr.config.ServerPreferenceConfig;
 import com.fr.design.constants.LayoutConstants;
+import com.fr.design.gui.itextarea.UITextArea;
 import com.fr.design.gui.itextfield.UITextField;
 import com.fr.design.layout.FRGUIPaneFactory;
 import com.fr.design.utils.gui.GUICoreUtils;
@@ -21,7 +22,7 @@ import java.awt.*;
 public class WebHyperNorthPane extends AbstractHyperNorthPane<WebHyperlink> {
     private UITextField itemNameTextField;
     private boolean needRenamePane = false;
-    private UITextField urlTextField;
+    private UITextArea urlTextField;
 
     public WebHyperNorthPane(boolean needRenamePane) {
         this.needRenamePane = needRenamePane;
@@ -45,15 +46,10 @@ public class WebHyperNorthPane extends AbstractHyperNorthPane<WebHyperlink> {
     protected JPanel setHeaderPanel() {
         JPanel headerPane = FRGUIPaneFactory.createBorderLayout_L_Pane();
 
-        JPanel urlPane = FRGUIPaneFactory.createX_AXISBoxInnerContainer_S_Pane();
-        urlTextField = new UITextField(24);
-        urlPane.add(urlTextField);
+        urlTextField = new UITextArea(1,headerPane.getWidth());
         urlTextField.setText(ProductConstants.WEBSITE_URL);
 
-        //UILabel label = new UILabel(Inter.getLocText(new String[]{"Example","Or"}, new String[]{":http://www.baidu.com","/main.jsp"}));
-        JPanel urlWithHelp = FRGUIPaneFactory.createNColumnGridInnerContainer_S_Pane(2);
-        urlWithHelp.add(GUICoreUtils.createNamedPane(urlPane, "URL:"));
-        //urlWithHelp.add(label);
+        JPanel urlWithHelp = GUICoreUtils.createNamedPane(urlTextField, "URL:");
 
         if (this.needRenamePane) {
             headerPane.setLayout(new BorderLayout(LayoutConstants.VGAP_LARGE, LayoutConstants.VGAP_SMALL));
