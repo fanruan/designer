12b5356c1251bf34a439930e50777d71532aea8d
zack <zack@finreport.com>
2018-06-21 17:06:08 +0800

    国际化

--- designer-base/src/com/fr/design/gui/iprogressbar/ProgressDialog.java
+++ designer-base/src/com/fr/design/gui/iprogressbar/ProgressDialog.java
@@ -48,7 +48,7 @@ public class ProgressDialog extends UIDialog {
         progressBar.setOpaque(false);
         progressBar.setBorder(null);
         panel.add(progressBar, BorderLayout.CENTER);
-        text = new UILabel(Inter.getLocText("FR-Designer_Loading_Project"), JLabel.CENTER);
+        text = new UILabel(Inter.getLocText("Fine-Designer_Loading_Project"), JLabel.CENTER);
         FRFont font = FRFont.getInstance().applySize(14).applyForeground(new ColorUIResource(333334));
         text.setFont(font);
         panel.add(text, BorderLayout.SOUTH);
