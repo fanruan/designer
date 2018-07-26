ba7ea6eda8b1b520689cc36a0d169a878cbf3186
richie <jihua.me@gmail.com>
2018-07-26 09:19:01 +0800

    换行符

--- designer-base/src/main/java/com/fr/design/gui/ilist/ListModelElement.java
+++ designer-base/src/main/java/com/fr/design/gui/ilist/ListModelElement.java
@@ -1,1 +1,19 @@
-package com.fr.design.gui.ilist;import com.fr.general.ComparatorUtils;import com.fr.stable.Nameable;public class ListModelElement {    public Nameable wrapper;    public ListModelElement(Nameable nameable) {        this.wrapper = nameable;    }    public boolean equals(Object o) {        return o instanceof ListModelElement                && ComparatorUtils.equals(((ListModelElement) o).wrapper, wrapper);    }}
+package com.fr.design.gui.ilist;
+
+import com.fr.general.ComparatorUtils;
+import com.fr.stable.Nameable;
+
+
+public class ListModelElement {
+
+    public Nameable wrapper;
+
+    public ListModelElement(Nameable nameable) {
+        this.wrapper = nameable;
+    }
+
+    public boolean equals(Object o) {
+        return o instanceof ListModelElement
+                && ComparatorUtils.equals(((ListModelElement) o).wrapper, wrapper);
+    }
+}
