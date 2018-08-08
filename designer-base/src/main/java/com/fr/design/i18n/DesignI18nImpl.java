1a6a195ee37da75ecf2428a8feed29ce58252364
richie <jihua.me@gmail.com>
2018-08-08 12:16:29 +0800

    REPORT-10449 插件国际化

---
+++ designer-base/src/main/java/com/fr/design/i18n/DesignI18nImpl.java
@@ -0,0 +1,71 @@
+package com.fr.design.i18n;
+
+import com.fr.general.GeneralContext;
+import com.fr.general.log.MessageFormatter;
+import com.fr.locale.DesignI18nProvider;
+import com.fr.locale.InterProviderFactory;
+import com.fr.locale.LocaleBundle;
+import com.fr.locale.LocaleFiles;
+import com.fr.locale.LocaleManager;
+import com.fr.locale.impl.FineLocaleManager;
+
+import java.util.List;
+import java.util.Locale;
+
+public class DesignI18nImpl implements DesignI18nProvider {
+
+    private static DesignI18nImpl instance = new DesignI18nImpl();
+
+    public static DesignI18nImpl getInstance() {
+        return instance;
+    }
+
+    private LocaleManager localeManager = FineLocaleManager.create();
+
+    private DesignI18nImpl() {
+        addResource("com/fr/design/i18n/main");
+        InterProviderFactory.registerDesignI18nProvider(this);
+    }
+
+    @Override
+    public String i18nText(String key) {
+        return localeManager.getLocalBundle(GeneralContext.getLocale()).getText(localeManager, key);
+    }
+
+    @Override
+    public String i18nText(String key, Object... args) {
+        String format = InterProviderFactory.getProvider().getLocText(key);
+        MessageFormatter.FormattingTuple tuple = MessageFormatter.arrayFormat(format, args);
+        return localeManager.getLocalBundle(GeneralContext.getLocale()).getText(localeManager, tuple.getMessage());
+    }
+
+    @Override
+    public void addResource(String path) {
+        localeManager.addResource(path);
+    }
+
+    @Override
+    public void removeResource(String path) {
+        localeManager.removeResource(path);
+    }
+
+    @Override
+    public LocaleBundle getLocalBundle(Locale locale) {
+        return localeManager.getLocalBundle(locale);
+    }
+
+    @Override
+    public List<LocaleFiles> getReverseList() {
+        return localeManager.getReverseList();
+    }
+
+    @Override
+    public List<LocaleFiles> getList() {
+        return localeManager.getList();
+    }
+
+    @Override
+    public void clear() {
+        localeManager.clear();
+    }
+}
