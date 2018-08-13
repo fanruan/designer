c6d6424a3faabdd989a3f140ad3cc688abe29470
Harrison <Harrison@fanruan.com>
2018-08-13 16:23:52 +0800

    REPORT-10025 10.0 & 5.0版本依据国际化规范调整国际化内容

--- designer-base/src/main/java/com/fr/design/hyperlink/WebHyperlinkPane.java
+++ designer-base/src/main/java/com/fr/design/hyperlink/WebHyperlinkPane.java
@@ -45,7 +45,7 @@ public class WebHyperlinkPane extends AbstractHyperLinkPane<WebHyperlink> {
         parameterViewPane.setBorder(GUICoreUtils.createTitledBorder(com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Basic_Parameters"), null));
 
         useCJKCheckBox = new UICheckBox(com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Basic_Hyperlink_Use_CJK_To_Encode_Parameter"));
-        extendParametersCheckBox = new UICheckBox(com.fr.design.i18n.Toolkit.i18nText("Find-Design_Basic_Hyperlink_Extends_Report_Parameters"));
+        extendParametersCheckBox = new UICheckBox(com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Basic_Hyperlink_Extends_Report_Parameters"));
         this.add(GUICoreUtils.createFlowPane(new Component[]{useCJKCheckBox, extendParametersCheckBox}, FlowLayout.LEFT), BorderLayout.SOUTH);
     }
 
