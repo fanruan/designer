a0abc164e99b40ef042cec4c2d4d4cb4713bb2fd
kerry <kerry@finereport.com>
2018-08-22 09:39:02 +0800

    无

---
+++ designer-base/src/main/java/com/fr/design/preview/MobilePreview.java
@@ -0,0 +1,48 @@
+package com.fr.design.preview;
+
+import com.fr.design.fun.impl.AbstractPreviewProvider;
+import com.fr.general.Inter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author kerry
+ * @date 2018/5/11
+ */
+public class MobilePreview extends AbstractPreviewProvider {
+    private static final int PREVIEW_TYPE = 4;
+    private static final String ACTION_TYPE = "path";
+
+    @Override
+    public String nameForPopupItem() {
+        return Inter.getLocText("Fine-Engine_Mobile_Preview");
+    }
+
+    @Override
+    public String iconPathForPopupItem() {
+        return "com/fr/design/images/buttonicon/mobile.png";
+    }
+
+    @Override
+    public String iconPathForLarge() {
+        return "com/fr/design/images/buttonicon/mobileb24.png";
+    }
+
+    @Override
+    public int previewTypeCode() {
+        return PREVIEW_TYPE;
+    }
+
+    @Override
+    public String getActionType() {
+        return ACTION_TYPE;
+    }
+
+    @Override
+    public Map<String, Object> parametersForPreview() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("op", "mobile");
+        return map;
+    }
+}
