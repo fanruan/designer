1949b9bb62c14b6c7d2e6dc11bb385f067ed02ef
zack <zack@finreport.com>
2018-09-06 14:51:04 +0800

    REPORT-9679 去除国际化影响,后续feature合进来之后统一定义成常量

--- designer-realize/src/main/java/com/fr/start/module/DesignerActivator.java
+++ designer-realize/src/main/java/com/fr/start/module/DesignerActivator.java
@@ -252,11 +252,11 @@ public class DesignerActivator extends Activator {
 
     private static NameableCreator[] hyperlinkTypes() {
         return new NameableCreator[]{
-                new NameObjectCreator(com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Basic_Hyperlink_Reportlet"), ReportletHyperlink.class, ReportletHyperlinkPane.ChartNoRename.class),
-                new NameObjectCreator(com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Basic_Email"), EmailJavaScript.class, EmailPane.class),
-                new NameObjectCreator(com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Basic_Hyperlink_Web_Link"), WebHyperlink.class, WebHyperlinkPane.ChartNoRename.class),
-                new NameObjectCreator(com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Basic_JavaScript_Dynamic_Parameters"), ParameterJavaScript.class, ParameterJavaScriptPane.ChartNoRename.class),
-                new NameObjectCreator(com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Report_JavaScript"), JavaScriptImpl.class, JavaScriptImplPane.ChartNoRename.class)
+                new NameObjectCreator("Fine-Design_Basic_Hyperlink_Reportlet", ReportletHyperlink.class, ReportletHyperlinkPane.ChartNoRename.class),
+                new NameObjectCreator("Fine-Design_Basic_Email", EmailJavaScript.class, EmailPane.class),
+                new NameObjectCreator("Fine-Design_Basic_Hyperlink_Web_Link", WebHyperlink.class, WebHyperlinkPane.ChartNoRename.class),
+                new NameObjectCreator("Fine-Design_Basic_JavaScript_Dynamic_Parameters", ParameterJavaScript.class, ParameterJavaScriptPane.ChartNoRename.class),
+                new NameObjectCreator("Fine-Design_Report_JavaScript", JavaScriptImpl.class, JavaScriptImplPane.ChartNoRename.class)
         };
     }
 
