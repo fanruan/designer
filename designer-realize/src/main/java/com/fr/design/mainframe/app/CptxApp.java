79dba16bf94716c5212dd1fb063708931757c610
yaoh.wu <yaoh.wu@fanruan.com>
2018-09-16 09:44:26 +0800

    REPORT-11352 远程设计去除socketio的适配，部分代码质量

--- designer-realize/src/main/java/com/fr/design/mainframe/app/CptxApp.java
+++ designer-realize/src/main/java/com/fr/design/mainframe/app/CptxApp.java
@@ -3,8 +3,10 @@ package com.fr.design.mainframe.app;
 import com.fr.base.extension.FileExtension;
 import com.fr.base.frpx.exception.FRPackageRunTimeException;
 import com.fr.base.frpx.exception.InvalidWorkBookException;
+import com.fr.design.i18n.Toolkit;
+import com.fr.exception.PermissionDeniedException;
+import com.fr.exception.TplLockedException;
 import com.fr.file.FILE;
-
 import com.fr.log.FineLoggerFactory;
 import com.fr.main.impl.WorkBook;
 import com.fr.main.impl.WorkBookAdapter;
@@ -16,32 +18,38 @@ import java.io.InputStream;
  * Created by juhaoyu on 2018/6/27.
  */
 class CptxApp extends AbstractWorkBookApp {
-    
+
     @Override
     public String[] defaultExtensions() {
-        
+
         return new String[]{FileExtension.CPTX.getExtension()};
     }
-    
+
     @Override
     public WorkBook asIOFile(FILE file) {
-        
+
         FineLoggerFactory.getLogger().info(com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Report_Template_Opening_And_Waiting", file.getName()) + "...");
-        WorkBookX tpl;
+        WorkBookX tpl = new WorkBookX();
         InputStream inputStream;
         try {
             inputStream = file.asInputStream();
             long time = System.currentTimeMillis();
             tpl = new WorkBookX(inputStream);
             FineLoggerFactory.getLogger().error("cost: " + (System.currentTimeMillis() - time) + " ms");
+
+
+        } catch (PermissionDeniedException exp) {
+            FineLoggerFactory.getLogger().error(Toolkit.i18nText("Fine-Design_Basic_Template_Permission_Denied") + file, exp);
+        } catch (TplLockedException exp) {
+            FineLoggerFactory.getLogger().error(file + Toolkit.i18nText("Fine-Design_Basic_Template_Status_Locked"), exp);
         } catch (Exception exp) {
             if (exp instanceof FRPackageRunTimeException) {
                 throw (FRPackageRunTimeException) exp;
             }
             throw new InvalidWorkBookException(file + ":" + exp.getMessage(), exp);
         }
-        
-        
+
+
         return new WorkBookAdapter(tpl);
     }
 }
