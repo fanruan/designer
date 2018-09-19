611df1e0ab70d8d78035c35ba8f5070deb335710
alex.sung <alex.sung@fanruan.com>
2018-09-19 18:18:27 +0800

    UT

---
+++ designer-realize/src/test/java/com.fr/design/mainframe/alphafine/component/AlphaFineDialogTest.java
@@ -0,0 +1,33 @@
+package com.fr.design.mainframe.alphafine.component;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AlphaFineDialogTest {
+
+//    AlphaFineDialog alphaFineDialog;
+
+    @Before
+    public void setUp() {
+//        alphaFineDialog = EasyMock.createMock(AlphaFineDialog.class);
+
+    }
+
+
+    @Test
+    public void doNormalSearchTest() {
+//        try {
+//            EasyMock.expect(AlphaFineConfigManager.isALPHALicAvailable()).andReturn(true);
+//            AlphaFineHelper.showAlphaFineDialog(false);
+//            Method format = AlphaFineDialog.class.getDeclaredMethod("doNormalSearch", String.class);
+//            format.setAccessible(true);
+//            EasyMock.expect(alphaFineDialog.getSegmentationResult()).andReturn(new String[]{"数据集"});
+//            format.invoke(alphaFineDialog, "数据集");
+////            EasyMock.replay(alphaFineDialog);
+//            Assert.assertNotNull(alphaFineDialog.getSegmentationResult());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+    }
+}
