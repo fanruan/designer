4765eb247f42422b8f7a005522e778819042b858
Mata.Li <Mata.Li@fanruan.com>
2018-10-31 20:12:57 +0800

    REPORT-11831

--- designer-base/src/main/java/com/fr/design/onlineupdate/ui/dialog/UpdateMainDialog.java
+++ designer-base/src/main/java/com/fr/design/onlineupdate/ui/dialog/UpdateMainDialog.java
@@ -23,11 +23,7 @@ import com.fr.design.onlineupdate.ui.widget.UpdateInfoTableCellRender;
 import com.fr.design.onlineupdate.ui.widget.UpdateInfoTableModel;
 import com.fr.design.onlineupdate.ui.widget.UpdateInfoTextAreaCellRender;
 import com.fr.design.utils.gui.GUICoreUtils;
-import com.fr.general.ComparatorUtils;
-import com.fr.general.DateUtils;
-import com.fr.general.GeneralUtils;
-import com.fr.general.IOUtils;
-import com.fr.general.SiteCenter;
+import com.fr.general.*;
 import com.fr.general.http.HttpClient;
 import com.fr.json.JSONArray;
 import com.fr.json.JSONObject;
@@ -346,8 +342,14 @@ public class UpdateMainDialog extends UIDialog {
         initUpdateActionPane();
 
         add(jarVersionInfoPane, BorderLayout.NORTH);
-        add(jarUpdateInfoPane, BorderLayout.CENTER);
-        add(updateActionPane, BorderLayout.SOUTH);
+
+        //海外版本不显示更新信息
+        if (GeneralContext.getLocale().equals(Locale.CHINA) || GeneralContext.getLocale().equals(Locale.TAIWAN)){
+            add(jarUpdateInfoPane, BorderLayout.CENTER);
+            add(updateActionPane, BorderLayout.SOUTH);
+        }else {
+            add(updateActionPane, BorderLayout.CENTER);
+        }
 
         addActionListenerForUpdateBtn();
 
