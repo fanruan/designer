82c1c501287dc8d6a815c2ad1526ce9f85d5fce0
kerry <kerry@fanruan.com>
2018-12-06 15:41:47 +0800

    REPORT-13276 release传一份

--- designer-base/src/main/java/com/fr/design/bbs/BBSLoginUtils.java
+++ designer-base/src/main/java/com/fr/design/bbs/BBSLoginUtils.java
@@ -1,14 +1,8 @@
 package com.fr.design.bbs;
 
-import com.fr.config.BBSAttr;
-import com.fr.config.Configuration;
-import com.fr.config.MarketConfig;
+import com.fr.base.passport.FinePassportManager;
 import com.fr.log.FineLoggerFactory;
-import com.fr.stable.StringUtils;
-import com.fr.transaction.Configurations;
-import com.fr.transaction.Worker;
 
-import java.util.List;
 
 /**
  * Created by ibm on 2017/8/21.
@@ -16,62 +10,14 @@ import java.util.List;
 public class BBSLoginUtils {
 
     public static void bbsLogin(final String username, final String password) {
-        final BBSAttr bbsAttr = new BBSAttr();
-        bbsAttr.setBbsUsername(username);
-        bbsAttr.setBbsPassword(password);
         try {
-            Configurations.update(new Worker() {
-                @Override
-                public void run() {
-                    MarketConfig.getInstance().setBBsAttr(bbsAttr);
-                }
-                @Override
-                public Class<? extends Configuration>[] targets() {
-                    return new Class[]{MarketConfig.class};
-                }
-            });
-
-        } catch (Exception e) {
-            FineLoggerFactory.getLogger().error(e.getMessage(), e);
-        }
-    }
-
-    public static void bbsLogin(List<String> list) {
-        try {
-            String uid = list.get(0);
-            String username = list.get(1);
-            String password = list.get(2);
-            final BBSAttr bbsAttr = new BBSAttr();
-            bbsAttr.setBbsUsername(username);
-            bbsAttr.setBbsPassword(password);
-            bbsAttr.setBbsUid(Integer.parseInt(uid));
-            bbsAttr.setInShowBBsName(username);
-            Configurations.update(new Worker() {
-                @Override
-                public void run() {
-                    MarketConfig.getInstance().setBBsAttr(bbsAttr);
-                }
-                @Override
-                public Class<? extends Configuration>[] targets() {
-                    return new Class[]{MarketConfig.class};
-                }
-            });
-
+            FinePassportManager.getInstance().login(username, password);
         } catch (Exception e) {
             FineLoggerFactory.getLogger().error(e.getMessage(), e);
         }
     }
 
     public static void bbsLogout() {
-        final BBSAttr bbsAttr = new BBSAttr();
-        bbsAttr.setBbsUsername(StringUtils.EMPTY);
-        bbsAttr.setBbsPassword(StringUtils.EMPTY);
-        bbsAttr.setBbsUid(0);
-        bbsAttr.setInShowBBsName(StringUtils.EMPTY);
-        try {
-            MarketConfig.getInstance().setBBsAttr(bbsAttr);
-        } catch (Exception e) {
-            FineLoggerFactory.getLogger().error(e.getMessage(), e);
-        }
+        FinePassportManager.getInstance().logout();
     }
 }
