9eead4389df6551742e65a70223f67e108557647
neil <459208047@qq.com>
2018-12-27 15:29:06 +0800

    .

--- designer-base/src/main/java/com/fr/design/fun/JavaScriptActionProvider.java
+++ designer-base/src/main/java/com/fr/design/fun/JavaScriptActionProvider.java
@@ -2,12 +2,11 @@ package com.fr.design.fun;
 
 import com.fr.design.beans.FurtherBasicBeanPane;
 import com.fr.design.javascript.JavaScriptActionPane;
-import com.fr.design.mainframe.JTemplate;
 import com.fr.js.JavaScript;
 import com.fr.stable.fun.mark.Mutable;
 
 /**
- * 控件的事件扩展接口
+ * Created by zack on 2015/8/14.
  */
 public interface JavaScriptActionProvider extends Mutable{
 
@@ -15,21 +14,9 @@ public interface JavaScriptActionProvider extends Mutable{
 
     int CURRENT_LEVEL = 1;
 
-    /**
-     * 事件的界面
-     */
     FurtherBasicBeanPane<? extends JavaScript> getJavaScriptActionPane();
 
-    /**
-     * 这个界面在哪些类型模板设计的时候会出现
-     * @see com.fr.design.mainframe.JWorkBook
-     * @see com.fr.design.mainframe.JForm
-     */
-    boolean accept(JTemplate template);
-
-    @Deprecated
     FurtherBasicBeanPane<? extends JavaScript> getJavaScriptActionPane(JavaScriptActionPane pane);
 
-    @Deprecated
-    boolean isSupportType();
+	boolean isSupportType();
 }
