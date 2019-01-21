f2280372d0f08ed0878cf731fe500267159cd75d
yaoh.wu <yaoh.wu@fanruan.com>
2019-01-21 20:30:56 +0800

    line separator

--- designer-base/src/main/java/com/fr/design/parameter/ParaDefinitePane.java
+++ designer-base/src/main/java/com/fr/design/parameter/ParaDefinitePane.java
@@ -1,1 +1,24 @@
-package com.fr.design.parameter;import com.fr.base.Parameter;public interface ParaDefinitePane {    Parameter[] getNoRepeatParas(Parameter[] paras);    void setParameterArray(Parameter[] ps);    Parameter[] getParameterArray();    void refreshParameter();    boolean isWithQueryButton();    void addingParameter2Editor(Parameter p);    void addingParameter2EditorWithQueryButton(Parameter p);    void addingAllParameter2Editor();}
+package com.fr.design.parameter;
+
+import com.fr.base.Parameter;
+
+
+public interface ParaDefinitePane {
+
+    Parameter[] getNoRepeatParas(Parameter[] paras);
+
+    void setParameterArray(Parameter[] ps);
+
+    Parameter[] getParameterArray();
+
+    void refreshParameter();
+
+    boolean isWithQueryButton();
+
+    void addingParameter2Editor(Parameter p);
+
+    void addingParameter2EditorWithQueryButton(Parameter p);
+
+    void addingAllParameter2Editor();
+
+}
