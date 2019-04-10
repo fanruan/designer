41c3d413deb7e7a278dc546b4ac39d08e013e4db
plough <plough@fanruan.com>
2019-04-10 13:48:54 +0800

    REPORT-14865 更新日志推送=>实现弹窗逻辑的核心类

--- designer-base/src/main/java/com/fr/design/onlineupdate/push/DesignerPushUpdateManager.java
+++ designer-base/src/main/java/com/fr/design/onlineupdate/push/DesignerPushUpdateManager.java
@@ -1,6 +1,10 @@
 package com.fr.design.onlineupdate.push;
 
+import com.fr.general.CloudCenter;
 import com.fr.general.GeneralContext;
+import com.fr.general.GeneralUtils;
+import com.fr.general.http.HttpClient;
+import com.fr.json.JSONObject;
 import com.fr.workspace.WorkContext;
 
 /**
@@ -8,10 +12,11 @@ import com.fr.workspace.WorkContext;
  */
 public class DesignerPushUpdateManager {
     private static DesignerPushUpdateManager singleton;
-//    private DesignerUpdateInfo updateInfo;
+    private DesignerUpdateInfo updateInfo;
+    private DesignerPushUpdateConfigManager config;
 
     private DesignerPushUpdateManager() {
-
+        config = DesignerPushUpdateConfigManager.getInstance();
     }
 
     public static DesignerPushUpdateManager getInstance() {
@@ -21,19 +26,49 @@ public class DesignerPushUpdateManager {
         return singleton;
     }
 
+    private void initUpdateInfo() {
+        String currentVersion = GeneralUtils.readFullBuildNO();
+
+        // todo：耗时请求，可能需要优化
+        HttpClient hc = new HttpClient(CloudCenter.getInstance().acquireUrlByKind("jar10.update"));
+        String latestVersion = new JSONObject(hc.getResponseText()).optString("versionNO");
+
+        String updatePushInfo = CloudCenter.getInstance().acquireUrlByKind("update.push");
+        JSONObject pushData = new JSONObject(updatePushInfo);
+
+        updateInfo = new DesignerUpdateInfo(currentVersion, latestVersion, pushData);
+    }
+
     /**
      * "自动更新推送"选项是否生效
      */
     public boolean isAutoPushUpdateSupported() {
-        // 远程设计和非中文环境，都不生效
-        return WorkContext.getCurrent().isLocal() && GeneralContext.isChineseEnv();
+        boolean isLocalEnv = WorkContext.getCurrent().isLocal();
+        boolean isChineseEnv = GeneralContext.isChineseEnv();
+
+        return isAutoPushUpdateSupported(isLocalEnv, isChineseEnv);
     }
+
+    private boolean isAutoPushUpdateSupported(boolean isLocalEnv, boolean isChineseEnv) {
+        // 远程设计和非中文环境，都不生效
+        return isLocalEnv && isChineseEnv;
+    }
+
     /**
      * 检查更新，如果有合适的更新版本，则弹窗
      */
-//    public void checkAndPop() {
-//        updateInfo.
-//    }
+    public void popUpDialog() {
+        if (!shouldPopUp()) {
+            return;
+        }
+        // todo: do pop
+    }
 
+    private boolean shouldPopUp() {
+        if (updateInfo == null) {
+            initUpdateInfo();
+        }
 
+        return isAutoPushUpdateSupported() && updateInfo.hasNewPushVersion(config.getLastIgnoredVersion());
+    }
 }
---
+++ designer-base/src/main/java/com/fr/design/onlineupdate/push/DesignerUpdateInfo.java
@@ -0,0 +1,84 @@
+package com.fr.design.onlineupdate.push;
+
+import com.fr.general.ComparatorUtils;
+import com.fr.json.JSONObject;
+import com.fr.stable.StringUtils;
+
+import java.security.InvalidParameterException;
+
+/**
+ * Created by plough on 2019/4/8.
+ */
+class DesignerUpdateInfo {
+    private static final String KEY_VERSION = "version";
+    private static final String KEY_CONTENT = "content";
+    private static final String KEY_BACKGROUND_URL = "background";
+    private static final String KEY_MORE_INFO_URL = "more";
+
+    private final String currentVersion;  // 当前版本
+    private final String latestVersion;  // 最新版本
+
+    private final String pushVersion;  // 推送版本
+    private final String pushContent;  // 推送更新内容
+    private final String backgroundUrl;  // 推送背景图片 url
+    private final String moreInfoUrl;  // 更多新特性
+
+    DesignerUpdateInfo(String currentVersion, String latestVersion, JSONObject pushData) {
+        this.currentVersion = currentVersion;
+        this.latestVersion = latestVersion;
+
+        this.pushVersion = pushData.optString(KEY_VERSION);
+        this.pushContent = pushData.optString(KEY_CONTENT);
+        this.backgroundUrl = pushData.optString(KEY_BACKGROUND_URL);
+        this.moreInfoUrl = pushData.optString(KEY_MORE_INFO_URL);
+
+        // 简单做下参数校验
+        if (hasEmptyField()) {
+            throw new InvalidParameterException();
+        }
+    }
+
+    private boolean hasEmptyField() {
+        return StringUtils.isEmpty(currentVersion)
+                || StringUtils.isEmpty(latestVersion)
+                || StringUtils.isEmpty(pushVersion)
+                || StringUtils.isEmpty(pushContent)
+                || StringUtils.isEmpty(backgroundUrl)
+                || StringUtils.isEmpty(moreInfoUrl);
+    }
+
+    String getCurrentVersion() {
+        return currentVersion;
+    }
+
+    String getPushVersion() {
+        return pushVersion;
+    }
+
+    String getLatestVersion() {
+        return latestVersion;
+    }
+
+    String getPushContent() {
+        return pushContent;
+    }
+
+    String getBackgroundUrl() {
+        return backgroundUrl;
+    }
+
+    String getMoreInfoUrl() {
+        return moreInfoUrl;
+    }
+
+    boolean hasNewPushVersion(String lastIgnoredVersion) {
+        boolean result = ComparatorUtils.compare(pushVersion, currentVersion) > 0
+                && ComparatorUtils.compare(pushVersion, latestVersion) <= 0;
+        if (StringUtils.isNotEmpty(lastIgnoredVersion)) {
+            result = result && ComparatorUtils.compare(pushVersion, lastIgnoredVersion) > 0;
+        }
+
+        return result;
+    }
+
+}
