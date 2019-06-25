210d02fc79f54fdb8742f48239194bc7af331f3d
neil <459208047@qq.com>
2019-06-25 18:25:19 +0800

    ct

--- designer-realize/src/main/java/com/fr/start/SplashContext.java
+++ designer-realize/src/main/java/com/fr/start/SplashContext.java
@@ -1,6 +1,5 @@
 package com.fr.start;
 
-import com.fr.concurrent.NamedThreadFactory;
 import com.fr.design.i18n.Toolkit;
 import com.fr.design.locale.impl.SplashMark;
 import com.fr.design.mainframe.bbs.BBSConstants;
