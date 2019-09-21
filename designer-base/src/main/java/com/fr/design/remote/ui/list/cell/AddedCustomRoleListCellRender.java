2d417386939eb6c3cb964749beee92f8abce91f6
author <author@corpmail.com>
2019-09-21 13:26:09 +0800

    REPORT-20549 远程设计改进（设计权限加上角色）

---
+++ designer-base/src/main/java/com/fr/design/remote/ui/list/cell/AddedCustomRoleListCellRender.java
@@ -0,0 +1,24 @@
+package com.fr.design.remote.ui.list.cell;
+
+import com.fr.base.BaseUtils;
+import com.fr.design.remote.ui.list.AddedMemberListCellRender;
+import com.fr.workspace.server.authority.RemoteDesignMember;
+
+import javax.swing.Icon;
+
+/**
+ * @author Lucian.Chen
+ * @version 10.0
+ * Created by Lucian.Chen on 2019/9/19
+ */
+public class AddedCustomRoleListCellRender extends AddedMemberListCellRender {
+    @Override
+    protected Icon getMemberIcon() {
+        return BaseUtils.readIcon("com/fr/design/remote/images/icon_Custom_Role_normal@1x.png");
+    }
+
+    @Override
+    protected String getMemberName(RemoteDesignMember member) {
+        return member.getUsername();
+    }
+}
---
+++ designer-base/src/main/java/com/fr/design/remote/ui/list/cell/AuthorityCustomRoleListCellRender.java
@@ -0,0 +1,24 @@
+package com.fr.design.remote.ui.list.cell;
+
+import com.fr.base.BaseUtils;
+import com.fr.design.remote.ui.list.AuthorityListCellRenderer;
+import com.fr.report.DesignAuthority;
+
+import javax.swing.Icon;
+
+/**
+ * @author Lucian.Chen
+ * @version 10.0
+ * Created by Lucian.Chen on 2019/9/19
+ */
+public class AuthorityCustomRoleListCellRender extends AuthorityListCellRenderer {
+    @Override
+    protected Icon getMemberIcon() {
+        return BaseUtils.readIcon("com/fr/design/remote/images/icon_Custom_Role_normal@1x.png");
+    }
+
+    @Override
+    protected String getMemberName(DesignAuthority authority) {
+        return authority.getUsername();
+    }
+}
