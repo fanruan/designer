355d5ebfd119a5a9506ff37015471e26d2f8f561
richie <richie@fanruan.com>
2019-09-24 16:01:32 +0800

    无JIRA任务 比如恶心的插件接口，稍微改下

--- designer-base/src/main/java/com/fr/design/report/AbstractExportPane.java
+++ designer-base/src/main/java/com/fr/design/report/AbstractExportPane.java
@@ -1,11 +1,11 @@
 package com.fr.design.report;
 
-import com.fr.design.dialog.BasicPane;
+import com.fr.design.beans.BasicStorePane;
 
 /**
  * Created by vito on 16/5/5.
  */
-public abstract class AbstractExportPane extends BasicPane {
+public abstract class AbstractExportPane extends BasicStorePane<Object> {
 
     public abstract void populate(Object t);
 
