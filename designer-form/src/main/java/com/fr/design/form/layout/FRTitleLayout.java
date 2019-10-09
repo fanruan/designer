3167f4339aa8ed5223af6234de8c742d93b3fdb1
author <author@corpmail.com>
2019-10-09 11:38:35 +0800

    无jira任务,sonar问题修复

--- designer-form/src/main/java/com/fr/design/form/layout/FRTitleLayout.java
+++ designer-form/src/main/java/com/fr/design/form/layout/FRTitleLayout.java
@@ -126,9 +126,9 @@ public class FRTitleLayout implements FRLayoutManager, LayoutManager{
 			int titleH = title==null ? 0 : WTitleLayout.TITLE_HEIGHT;
 			for (int i=0; i< target.getComponentCount(); i++) {
 				Component comp = target.getComponent(i);
-				if (comp == title) {
+				if (comp != null && comp == title) {
 					comp.setBounds(0, 0, width, WTitleLayout.TITLE_HEIGHT);
-				} else if (comp == body) {
+				} else if (comp != null && comp == body) {
 					int y = titleH+gap;
 					comp.setBounds(0, y, width, height-y);
 				}
