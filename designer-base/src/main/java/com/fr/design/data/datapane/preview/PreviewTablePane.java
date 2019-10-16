30a1b542280da66ab4c9827fd6179db68e80368a
hades <hades@fanraun.com>
2019-10-16 13:04:35 +0800

    update

--- designer-base/src/main/java/com/fr/design/data/datapane/preview/PreviewTablePane.java
+++ designer-base/src/main/java/com/fr/design/data/datapane/preview/PreviewTablePane.java
@@ -40,7 +40,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
