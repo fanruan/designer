3f5f9b40dbc5e7df7c94ddaa6cb5344d851adddc
Joe.Jiang <Joe.Jiang@fanruan.com>
2019-10-21 15:03:20 +0800

    REPORT-21592 单元格属性-形态-数据字典，选择一个需要内网连接的数据库，一直在加载，没有加载成功或者失败的弹框

--- designer-base/src/main/java/com/fr/design/data/datapane/ChoosePane.java
+++ designer-base/src/main/java/com/fr/design/data/datapane/ChoosePane.java
@@ -438,8 +438,7 @@ public class ChoosePane extends BasicBeanPane<DataBaseItems> implements Refresha
             }
         } catch (Exception e) {
             FineLoggerFactory.getLogger().error(e.getMessage(), e);
-            DesignerFrame designerFrame = DesignerContext.getDesignerFrame();
-            JOptionPane.showMessageDialog(designerFrame, com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Basic_Database_Connection_Failed"),
+            JOptionPane.showMessageDialog(DesignerContext.getDesignerFrame(), com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Basic_Database_Connection_Failed"),
                     com.fr.design.i18n.Toolkit.i18nText("Fine-Design_Basic_Failed"), JOptionPane.ERROR_MESSAGE);
         }
     }
