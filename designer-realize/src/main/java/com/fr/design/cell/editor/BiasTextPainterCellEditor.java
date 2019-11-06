d8f99774674ced5aef35f768c99ffd6a5cd6fb46
hades <hades@fanraun.com>
2019-11-06 11:53:37 +0800

    REPORT-23246 设计器UI 插入斜线显示不全

--- designer-realize/src/main/java/com/fr/design/cell/editor/BiasTextPainterCellEditor.java
+++ designer-realize/src/main/java/com/fr/design/cell/editor/BiasTextPainterCellEditor.java
@@ -10,6 +10,7 @@ import com.fr.design.gui.ilable.UILabel;
 import com.fr.design.gui.itextarea.UITextArea;
 import com.fr.design.layout.FRGUIPaneFactory;
 import com.fr.design.mainframe.ElementCasePane;
+import com.fr.design.utils.gui.FineDesignScreen;
 import com.fr.general.ComparatorUtils;
 import com.fr.grid.Grid;
 import com.fr.report.cell.TemplateCellElement;
@@ -35,7 +36,7 @@ import java.awt.event.ActionListener;
 public class BiasTextPainterCellEditor extends AbstractCellEditor {
 
     private BiasTextPainterPane biasTextPainterPane = null;
-    private static final double MULTIPLE = 1.5;
+    private static final double MULTIPLE = FineDesignScreen.isHighDPI() ? 2 : 1.5;
 
     /**
      * Constructor.
@@ -114,7 +115,7 @@ public class BiasTextPainterCellEditor extends AbstractCellEditor {
 
             //center
             JPanel centerPane =FRGUIPaneFactory.createBorderLayout_S_Pane();
-            defaultPane.add(centerPane, BorderLayout.CENTER);
+            defaultPane.add(centerPane, BorderLayout.NORTH);
 
             //text
             JPanel textPane = FRGUIPaneFactory.createBorderLayout_S_Pane();
