f8329a6aff5a80f88501def038de64315f3bd408
hades <hades@fanraun.com>
2020-02-25 18:02:23 +0800

    REPORT-27138 && REPORT-27137 && REPORT-27109 && REPORT-27140

--- designer-realize/src/main/java/com/fr/start/Designer.java
+++ designer-realize/src/main/java/com/fr/start/Designer.java
@@ -1,12 +1,5 @@
 package com.fr.start;
 
-import com.fr.event.Event;
-import com.fr.event.Listener;
-import com.fr.event.Null;
-import com.fr.process.FineProcess;
-import com.fr.process.engine.FineJavaProcessFactory;
-import com.fr.process.engine.core.FineProcessEngineEvent;
-
 /**
  * 设计器主进程入口(无缝更换升级jar包，若使用其他类作为入口，需要重新打包designer.exe等，升级后仍然走的原来逻辑)
  *
@@ -19,21 +12,6 @@ public class Designer {
 
     public static void main(String[] args) {
         // 创建进程
-        final FineProcess process = FineJavaProcessFactory.create().
-                entry("com.fr.start.MainDesigner").
-                javaRuntime(DesignerJavaRuntime.getInstance().getJavaExec()).
-                classPath(DesignerJavaRuntime.getInstance().getClassPath()).
-                inheritJvmSettings().
-                jvmSettings(DesignerJavaRuntime.getInstance().getJvmOptions()).
-                arguments(args).
-                startProcess(DesignerProcessType.INSTANCE);
-
-        process.getPipe().listen(FineProcessEngineEvent.DESTROY, new Listener<Null>() {
-            @Override
-            public void on(Event event, Null param) {
-                process.destroy();
-            }
-        });
-
+        DesignerLauncher.getInstance().start(args);
     }
 }
