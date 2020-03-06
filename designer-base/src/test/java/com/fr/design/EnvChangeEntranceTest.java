b9e13fbef351a6282f25c71e596c4f940cafc957
Maksim <Maksim@fanruan.com>
2020-03-06 11:31:52 +0800

    测试

--- designer-base/src/test/java/com/fr/design/EnvChangeEntranceTest.java
+++ designer-base/src/test/java/com/fr/design/EnvChangeEntranceTest.java
@@ -6,6 +6,7 @@ import com.fr.design.write.submit.CheckServiceDialog;
 import com.fr.workspace.connect.WorkspaceConnectionInfo;
 import com.fr.workspace.engine.channel.http.FunctionalHttpRequest;
 import org.easymock.EasyMock;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.easymock.PowerMock;
@@ -23,30 +24,35 @@ public class EnvChangeEntranceTest {
 
     @Test
     public void showServiceDialog() throws Exception {
-        EnvChangeEntrance entrance = EnvChangeEntrance.getInstance();
-        DesignerWorkspaceInfo selectedEnv = EasyMock.mock(DesignerWorkspaceInfo.class);
-        WorkspaceConnectionInfo connectionInfo = EasyMock.mock(WorkspaceConnectionInfo.class);
+        try {
+            EnvChangeEntrance entrance = EnvChangeEntrance.getInstance();
+            DesignerWorkspaceInfo selectedEnv = EasyMock.mock(DesignerWorkspaceInfo.class);
+            WorkspaceConnectionInfo connectionInfo = EasyMock.mock(WorkspaceConnectionInfo.class);
 
-        String remoteBranch = "Build#persist-2020.02.15.01.01.12.12";
-        EasyMock.expect(selectedEnv.getConnection()).andReturn(connectionInfo);
-        EasyMock.expect(selectedEnv.getType()).andReturn(DesignerWorkspaceType.Remote);
-        EasyMock.expect(selectedEnv.getRemindTime()).andReturn(null);
+            String remoteBranch = "Build#persist-2020.02.15.01.01.12.12";
+            EasyMock.expect(selectedEnv.getConnection()).andReturn(connectionInfo);
+            EasyMock.expect(selectedEnv.getType()).andReturn(DesignerWorkspaceType.Remote);
+            EasyMock.expect(selectedEnv.getRemindTime()).andReturn(null);
 
-        FunctionalHttpRequest request = EasyMock.mock(FunctionalHttpRequest.class);
-        EasyMock.expect(request.getServiceList()).andReturn(null);
+            FunctionalHttpRequest request = EasyMock.mock(FunctionalHttpRequest.class);
+            EasyMock.expect(request.getServiceList()).andReturn(null);
 
-        PowerMock.expectNew(FunctionalHttpRequest.class,connectionInfo).andReturn(request).anyTimes();
-        EasyMock.expect(request.getServerBranch()).andReturn(remoteBranch);
+            PowerMock.expectNew(FunctionalHttpRequest.class, connectionInfo).andReturn(request).anyTimes();
+            EasyMock.expect(request.getServerBranch()).andReturn(remoteBranch);
 
-        CheckServiceDialog dialog = EasyMock.mock(CheckServiceDialog.class);
-        PowerMock.expectNew(CheckServiceDialog.class,EasyMock.anyObject(),EasyMock.anyObject(),EasyMock.anyObject(),EasyMock.anyObject()).andReturn(dialog);
+            CheckServiceDialog dialog = EasyMock.mock(CheckServiceDialog.class);
+            PowerMock.expectNew(CheckServiceDialog.class, EasyMock.anyObject(), EasyMock.anyObject(), EasyMock.anyObject(), EasyMock.anyObject()).andReturn(dialog);
 
-        EasyMock.replay(request);
-        EasyMock.replay(selectedEnv);
-        EasyMock.replay(connectionInfo);
-        PowerMock.replayAll();
+            EasyMock.replay(request);
+            EasyMock.replay(selectedEnv);
+            EasyMock.replay(connectionInfo);
+            PowerMock.replayAll();
 
-        entrance.showServiceDialog(selectedEnv);
+            entrance.showServiceDialog(selectedEnv);
+            Assert.assertTrue(true);
+        }catch (Exception e){
+            Assert.assertTrue(false);
+        }
     }
 
 }
